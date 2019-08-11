f59208e37cc632316115e9c826e923f5f250bb6c
sarita <sarita.chetak@gmail.com>
2019-08-11 11:25:51 +0100

    vanita

---
+++ src/javaProgram/Mod.java
@@ -0,0 +1,12 @@
+package javaProgram;
+
+public class Mod {
+    public static void main(String[] args) {
+        int a,b,result;
+        a=30;
+        b=5;
+        result=a%b;
+        System.out.println("result=" +result);
+
+    }
+}
